a19eacae90088be54d45246fda29c5a548473f07
kraftbj <kraftbj@users.noreply.github.com>
2024-12-06 15:18:28 +0000

    js-packages: Fix some JS lints ahead of the eslint 9 upgrade (#40489)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/12201088977
    
    Upstream-Ref: Automattic/jetpack@8c69a5ea26fe8ea99d2f77435f048a6badee61ed

--- build/ai-client/src/hooks/use-ai-suggestions/index.d.ts
+++ build/ai-client/src/hooks/use-ai-suggestions/index.d.ts
@@ -3,8 +3,7 @@
  */
 import type { AskQuestionOptionsArgProps } from '../../ask-question/index.js';
 import type SuggestionsEventSource from '../../suggestions-event-source/index.js';
-import type { PromptProp, SuggestionErrorCode } from '../../types.js';
-import type { RequestingStateProp } from '../../types.js';
+import type { PromptProp, SuggestionErrorCode, RequestingStateProp } from '../../types.js';
 export type RequestingErrorProps = {
     code: SuggestionErrorCode;
     message: string;
