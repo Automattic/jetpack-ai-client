839027c6405b10db25075f91f4fea7e839876798
dhasilva <dhasilva@users.noreply.github.com>
2025-03-14 14:57:52 +0000

    SEO Enhancer: Auto-generate alt text for images if enabled (#42445)
    * move getBase64Image to libs and only use it on retry for image extension
    
    * add image busy state to seo enhancer store
    
    * update withAiImageExtension to check if an image is busy via the SEO store
    
    * update useSeoRequests to use base64 on retry, use the store and expose a function for a single image
    
    * extend the image block to auto-generate alt text
    
    * fix loading indicator
    
    * changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/13859010408
    
    Upstream-Ref: Automattic/jetpack@983bafe39c2b8a40ab11a781cff53404076d62bc

---
+++ build/libs/get-base64-image.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Get the base64 representation of an image.
+ *
+ * @param {string} url - The URL of the image.
+ * @return {Promise<string>} The base64 representation of the image.
+ */
+export declare const getBase64Image: (url: string) => Promise<string>;
