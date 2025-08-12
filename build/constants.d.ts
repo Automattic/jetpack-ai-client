88ab184648fd0dd14b6c5ba26740a081e8275cfb
manzoorwanijk <manzoorwanijk@users.noreply.github.com>
2025-08-12 05:49:49 +0000

    Update wordpress monorepo (#44701)
    * Update wordpress monorepo
    
    * Remove unneeded padding param
    
    * %s and %u -> %d
    
    * Unconfuse typing by moving replace() outside of sprintf()
    
    * Add object property to settings
    
    * Update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: tbradsha <32492176+tbradsha@users.noreply.github.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16899944782
    
    Upstream-Ref: Automattic/jetpack@2d926e9cfa029ea9b7c93dd89d000854f2c3897a

--- build/constants.d.ts
+++ build/constants.d.ts
@@ -1,80 +1,80 @@
 export declare const LANGUAGE_MAP: {
     en: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"English">;
+        label: import("@wordpress/i18n").TranslatableText<"English">;
     };
     es: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Spanish">;
+        label: import("@wordpress/i18n").TranslatableText<"Spanish">;
     };
     fr: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"French">;
+        label: import("@wordpress/i18n").TranslatableText<"French">;
     };
     de: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"German">;
+        label: import("@wordpress/i18n").TranslatableText<"German">;
     };
     it: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Italian">;
+        label: import("@wordpress/i18n").TranslatableText<"Italian">;
     };
     pt: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Portuguese">;
+        label: import("@wordpress/i18n").TranslatableText<"Portuguese">;
     };
     ru: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Russian">;
+        label: import("@wordpress/i18n").TranslatableText<"Russian">;
     };
     zh: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Chinese">;
+        label: import("@wordpress/i18n").TranslatableText<"Chinese">;
     };
     ja: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Japanese">;
+        label: import("@wordpress/i18n").TranslatableText<"Japanese">;
     };
     ar: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Arabic">;
+        label: import("@wordpress/i18n").TranslatableText<"Arabic">;
     };
     hi: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Hindi">;
+        label: import("@wordpress/i18n").TranslatableText<"Hindi">;
     };
     ko: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Korean">;
+        label: import("@wordpress/i18n").TranslatableText<"Korean">;
     };
 };
 export declare const PROMPT_TONES_MAP: {
     formal: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Formal">;
+        label: import("@wordpress/i18n").TranslatableText<"Formal">;
         emoji: string;
     };
     informal: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Informal">;
+        label: import("@wordpress/i18n").TranslatableText<"Informal">;
         emoji: string;
     };
     optimistic: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Optimistic">;
+        label: import("@wordpress/i18n").TranslatableText<"Optimistic">;
         emoji: string;
     };
     humorous: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Humorous">;
+        label: import("@wordpress/i18n").TranslatableText<"Humorous">;
         emoji: string;
     };
     serious: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Serious">;
+        label: import("@wordpress/i18n").TranslatableText<"Serious">;
         emoji: string;
     };
     skeptical: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Skeptical">;
+        label: import("@wordpress/i18n").TranslatableText<"Skeptical">;
         emoji: string;
     };
     empathetic: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Empathetic">;
+        label: import("@wordpress/i18n").TranslatableText<"Empathetic">;
         emoji: string;
     };
     confident: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Confident">;
+        label: import("@wordpress/i18n").TranslatableText<"Confident">;
         emoji: string;
     };
     passionate: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Passionate">;
+        label: import("@wordpress/i18n").TranslatableText<"Passionate">;
         emoji: string;
     };
     provocative: {
-        label: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Provocative">;
+        label: import("@wordpress/i18n").TranslatableText<"Provocative">;
         emoji: string;
     };
 };
@@ -92,18 +92,18 @@ export declare const PROMPT_TYPE_USER_PROMPT: "userPrompt";
 export declare const PROMPT_TYPE_JETPACK_FORM_CUSTOM_PROMPT: "jetpackFormCustomPrompt";
 export declare const PROMPT_TYPE_TRANSFORM_LIST_TO_TABLE: "transformListToTable";
 export declare const PROMPT_TYPE_WRITE_POST_FROM_LIST: "writePostFromList";
-export declare const TRANSLATE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Translate">;
-export declare const TONE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Change tone">;
-export declare const CORRECT_SPELLING_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Correct spelling and grammar">;
-export declare const SIMPLIFY_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Simplify">;
-export declare const SUMMARIZE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Summarize">;
-export declare const MAKE_SHORTER_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Make shorter">;
-export declare const MAKE_LONGER_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Expand">;
-export declare const TURN_LIST_INTO_TABLE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Turn list into a table">;
-export declare const WRITE_POST_FROM_LIST_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Write a post from this list">;
-export declare const GENERATE_TITLE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Generate a post title">;
-export declare const SUMMARY_BASED_ON_TITLE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Summary based on title">;
-export declare const CONTINUE_LABEL: import("@wordpress/i18n/build-types/types.js").TranslatableText<"Continue writing">;
+export declare const TRANSLATE_LABEL: import("@wordpress/i18n").TranslatableText<"Translate">;
+export declare const TONE_LABEL: import("@wordpress/i18n").TranslatableText<"Change tone">;
+export declare const CORRECT_SPELLING_LABEL: import("@wordpress/i18n").TranslatableText<"Correct spelling and grammar">;
+export declare const SIMPLIFY_LABEL: import("@wordpress/i18n").TranslatableText<"Simplify">;
+export declare const SUMMARIZE_LABEL: import("@wordpress/i18n").TranslatableText<"Summarize">;
+export declare const MAKE_SHORTER_LABEL: import("@wordpress/i18n").TranslatableText<"Make shorter">;
+export declare const MAKE_LONGER_LABEL: import("@wordpress/i18n").TranslatableText<"Expand">;
+export declare const TURN_LIST_INTO_TABLE_LABEL: import("@wordpress/i18n").TranslatableText<"Turn list into a table">;
+export declare const WRITE_POST_FROM_LIST_LABEL: import("@wordpress/i18n").TranslatableText<"Write a post from this list">;
+export declare const GENERATE_TITLE_LABEL: import("@wordpress/i18n").TranslatableText<"Generate a post title">;
+export declare const SUMMARY_BASED_ON_TITLE_LABEL: import("@wordpress/i18n").TranslatableText<"Summary based on title">;
+export declare const CONTINUE_LABEL: import("@wordpress/i18n").TranslatableText<"Continue writing">;
 export declare const PLACEMENT_JETPACK_SIDEBAR: "jetpack-sidebar";
 export declare const PLACEMENT_DOCUMENT_SETTINGS: "document-settings";
 export declare const PLACEMENT_PRE_PUBLISH: "pre-publish";
