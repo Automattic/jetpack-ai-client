f8db7917c5e40598c91a87f00c9b2116d3713e52
tbradsha <tbradsha@users.noreply.github.com>
2026-04-17 13:00:48 +0000

    Update wordpress monorepo (#48106)
    * Fix `@wordpress/ui` peer dep issue.
    * Charts: Rename WPDS font tokens to typography-* for @wordpress/theme@0.11.0
    * Fix TS errors
    * Clean up global types mess in mu-wpcom
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    Co-authored-by: Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
    Co-authored-by: Manzoor Wani <manzoorwani.jk@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24565711396
    
    Upstream-Ref: Automattic/jetpack@5cca8ab3cec81465ff7fcc8d169be4983f83d7c6

--- build/src/constants.d.ts
+++ build/src/constants.d.ts
@@ -1,80 +1,80 @@
 export declare const LANGUAGE_MAP: {
     en: {
-        label: import("@wordpress/i18n").TranslatableText<"English">;
+        label: import("@wordpress/i18n").TransformedText<"English">;
     };
     es: {
-        label: import("@wordpress/i18n").TranslatableText<"Spanish">;
+        label: import("@wordpress/i18n").TransformedText<"Spanish">;
     };
     fr: {
-        label: import("@wordpress/i18n").TranslatableText<"French">;
+        label: import("@wordpress/i18n").TransformedText<"French">;
     };
     de: {
-        label: import("@wordpress/i18n").TranslatableText<"German">;
+        label: import("@wordpress/i18n").TransformedText<"German">;
     };
     it: {
-        label: import("@wordpress/i18n").TranslatableText<"Italian">;
+        label: import("@wordpress/i18n").TransformedText<"Italian">;
     };
     pt: {
-        label: import("@wordpress/i18n").TranslatableText<"Portuguese">;
+        label: import("@wordpress/i18n").TransformedText<"Portuguese">;
     };
     ru: {
-        label: import("@wordpress/i18n").TranslatableText<"Russian">;
+        label: import("@wordpress/i18n").TransformedText<"Russian">;
     };
     zh: {
-        label: import("@wordpress/i18n").TranslatableText<"Chinese">;
+        label: import("@wordpress/i18n").TransformedText<"Chinese">;
     };
     ja: {
-        label: import("@wordpress/i18n").TranslatableText<"Japanese">;
+        label: import("@wordpress/i18n").TransformedText<"Japanese">;
     };
     ar: {
-        label: import("@wordpress/i18n").TranslatableText<"Arabic">;
+        label: import("@wordpress/i18n").TransformedText<"Arabic">;
     };
     hi: {
-        label: import("@wordpress/i18n").TranslatableText<"Hindi">;
+        label: import("@wordpress/i18n").TransformedText<"Hindi">;
     };
     ko: {
-        label: import("@wordpress/i18n").TranslatableText<"Korean">;
+        label: import("@wordpress/i18n").TransformedText<"Korean">;
     };
 };
 export declare const PROMPT_TONES_MAP: {
     formal: {
-        label: import("@wordpress/i18n").TranslatableText<"Formal">;
+        label: import("@wordpress/i18n").TransformedText<"Formal">;
         emoji: string;
     };
     informal: {
-        label: import("@wordpress/i18n").TranslatableText<"Informal">;
+        label: import("@wordpress/i18n").TransformedText<"Informal">;
         emoji: string;
     };
     optimistic: {
-        label: import("@wordpress/i18n").TranslatableText<"Optimistic">;
+        label: import("@wordpress/i18n").TransformedText<"Optimistic">;
         emoji: string;
     };
     humorous: {
-        label: import("@wordpress/i18n").TranslatableText<"Humorous">;
+        label: import("@wordpress/i18n").TransformedText<"Humorous">;
         emoji: string;
     };
     serious: {
-        label: import("@wordpress/i18n").TranslatableText<"Serious">;
+        label: import("@wordpress/i18n").TransformedText<"Serious">;
         emoji: string;
     };
     skeptical: {
-        label: import("@wordpress/i18n").TranslatableText<"Skeptical">;
+        label: import("@wordpress/i18n").TransformedText<"Skeptical">;
         emoji: string;
     };
     empathetic: {
-        label: import("@wordpress/i18n").TranslatableText<"Empathetic">;
+        label: import("@wordpress/i18n").TransformedText<"Empathetic">;
         emoji: string;
     };
     confident: {
-        label: import("@wordpress/i18n").TranslatableText<"Confident">;
+        label: import("@wordpress/i18n").TransformedText<"Confident">;
         emoji: string;
     };
     passionate: {
-        label: import("@wordpress/i18n").TranslatableText<"Passionate">;
+        label: import("@wordpress/i18n").TransformedText<"Passionate">;
         emoji: string;
     };
     provocative: {
-        label: import("@wordpress/i18n").TranslatableText<"Provocative">;
+        label: import("@wordpress/i18n").TransformedText<"Provocative">;
         emoji: string;
     };
 };
@@ -92,18 +92,18 @@ export declare const PROMPT_TYPE_USER_PROMPT: "userPrompt";
 export declare const PROMPT_TYPE_JETPACK_FORM_CUSTOM_PROMPT: "jetpackFormCustomPrompt";
 export declare const PROMPT_TYPE_TRANSFORM_LIST_TO_TABLE: "transformListToTable";
 export declare const PROMPT_TYPE_WRITE_POST_FROM_LIST: "writePostFromList";
-export declare const TRANSLATE_LABEL: import("@wordpress/i18n").TranslatableText<"Translate">;
-export declare const TONE_LABEL: import("@wordpress/i18n").TranslatableText<"Change tone">;
-export declare const CORRECT_SPELLING_LABEL: import("@wordpress/i18n").TranslatableText<"Correct spelling and grammar">;
-export declare const SIMPLIFY_LABEL: import("@wordpress/i18n").TranslatableText<"Simplify">;
-export declare const SUMMARIZE_LABEL: import("@wordpress/i18n").TranslatableText<"Summarize">;
-export declare const MAKE_SHORTER_LABEL: import("@wordpress/i18n").TranslatableText<"Make shorter">;
-export declare const MAKE_LONGER_LABEL: import("@wordpress/i18n").TranslatableText<"Expand">;
-export declare const TURN_LIST_INTO_TABLE_LABEL: import("@wordpress/i18n").TranslatableText<"Turn list into a table">;
-export declare const WRITE_POST_FROM_LIST_LABEL: import("@wordpress/i18n").TranslatableText<"Write a post from this list">;
-export declare const GENERATE_TITLE_LABEL: import("@wordpress/i18n").TranslatableText<"Generate a post title">;
-export declare const SUMMARY_BASED_ON_TITLE_LABEL: import("@wordpress/i18n").TranslatableText<"Summary based on title">;
-export declare const CONTINUE_LABEL: import("@wordpress/i18n").TranslatableText<"Continue writing">;
+export declare const TRANSLATE_LABEL: import("@wordpress/i18n").TransformedText<"Translate">;
+export declare const TONE_LABEL: import("@wordpress/i18n").TransformedText<"Change tone">;
+export declare const CORRECT_SPELLING_LABEL: import("@wordpress/i18n").TransformedText<"Correct spelling and grammar">;
+export declare const SIMPLIFY_LABEL: import("@wordpress/i18n").TransformedText<"Simplify">;
+export declare const SUMMARIZE_LABEL: import("@wordpress/i18n").TransformedText<"Summarize">;
+export declare const MAKE_SHORTER_LABEL: import("@wordpress/i18n").TransformedText<"Make shorter">;
+export declare const MAKE_LONGER_LABEL: import("@wordpress/i18n").TransformedText<"Expand">;
+export declare const TURN_LIST_INTO_TABLE_LABEL: import("@wordpress/i18n").TransformedText<"Turn list into a table">;
+export declare const WRITE_POST_FROM_LIST_LABEL: import("@wordpress/i18n").TransformedText<"Write a post from this list">;
+export declare const GENERATE_TITLE_LABEL: import("@wordpress/i18n").TransformedText<"Generate a post title">;
+export declare const SUMMARY_BASED_ON_TITLE_LABEL: import("@wordpress/i18n").TransformedText<"Summary based on title">;
+export declare const CONTINUE_LABEL: import("@wordpress/i18n").TransformedText<"Continue writing">;
 export declare const PLACEMENT_JETPACK_SIDEBAR: "jetpack-sidebar";
 export declare const PLACEMENT_DOCUMENT_SETTINGS: "document-settings";
 export declare const PLACEMENT_PRE_PUBLISH: "pre-publish";
--- build/src/hooks/use-post-content.d.ts
+++ build/src/hooks/use-post-content.d.ts
@@ -1,6 +1,6 @@
 declare const usePostContent: () => {
     getPostContent: (preprocess?: (serialized: string) => string) => string;
     isEditedPostEmpty: () => boolean;
-    getSerializedPostContent: () => any;
+    getSerializedPostContent: () => string;
 };
 export default usePostContent;
